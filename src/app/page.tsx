375c24c4a6266285fe38e8a00c1a5f4c311faef6
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-08-01 16:23:47 -0300

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 import construtor from "@/worker.webp";
+import mall from "@/mall.png";
+import ribeirao from "@/ribeirao.png";
+import bangu from "@/bangu.png";
+import letter from "@/letter.png";
 
 export default function Home() {
   return (
@@ -9,11 +13,8 @@ export default function Home() {
       <div className={styles.banner}>
         <div className={styles.bannerBox}>
           <div className={styles.bannerContent}>
-            <h1>Construtora</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quibusdam, quia,
-              quod voluptate
-            </p>
+            <h1>Construção</h1>
+            <p>Nossa construtora é mais do que um nome, é uma promessa de excelência em cada projeto.</p>
             <div className={styles.bannerButtons}>
               <button>Conheça nossos projetos</button>
               <button>Contato</button>
@@ -34,6 +35,30 @@ export default function Home() {
           </div>
           <div className={styles.groupImage}>
             <Image src={construtor} alt="construtor" width={500} />
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <div className={styles.parceiros}>
+        <div className={styles.parceirosContent}>
+          <Image src={mall} alt="parceiro" width={150} />
+          <Image src={bangu} alt="parceiro" width={150} />
+          <Image src={ribeirao} alt="parceiro" width={150} />
+          <Image src={letter} alt="parceiro" width={150} />
+        </div>
+      </div>
+      {/*  */}
+      <div className={styles.banner2}>
+        <div className={styles.bannerBox2}>
+          <div className={styles.bannerContent2}>
+            <h1>Confira nossos projetos</h1>
+            <p>
+              De imponentes arranha-céus a acolhedoras comunidades planejadas, nossos projetos refletem
+              inovação e qualidade, deixando uma marca inspiradora em suas áreas.
+            </p>
+            <div className={styles.bannerButtons2}>
+              <button>Acesse</button>
+            </div>
           </div>
         </div>
       </div>
